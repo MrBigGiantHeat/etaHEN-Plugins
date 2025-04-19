a0a9cb3bc1ddd0933edd132f932973d6def75b1d
MrBigGiantHeat <amr.aus2022@gmail.com>
2025-04-19 16:21:25 +1000

    f

--- daemon/source/elfserver.cpp
+++ daemon/source/elfserver.cpp
@@ -112,7 +112,7 @@ static TitleId getNextAvailableTitleId() noexcept {
 }
 
 static bool killApp(uint32_t appId) {
-	uint32_t res = sceLncUtilKillApp(appId);
+	[[maybe_unused]]	uint32_t res = sceLncUtilKillApp(appId);
 	// printf("sceApplicationKill returned 0x%llx\n", res);
 	return true;
 }
